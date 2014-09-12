62e26f220461940b078c759224f4a699603a6982
MetalMatze <me@metalmatze.de>
2014-09-13 01:32:21 +0200

    Use 127.0.0.1 to connect to travis' mysql

--- app/config/travis/database.php
+++ app/config/travis/database.php
@@ -9,7 +9,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => '127.0.0.1',
 			'database'  => 'phototresor_test',
 			'username'  => 'root',
 			'password'  => '',
