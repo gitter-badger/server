e2d37e9f7a706cc954b7e066610bb25fa36dd266
MetalMatze <me@metalmatze.de>
2014-09-13 22:02:52 +0200

    Remove travis env and use testing

--- app/config/testing/database.php
+++ app/config/testing/database.php
@@ -10,7 +10,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'phototresor',
+			'database'  => 'phototresor_testing',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
