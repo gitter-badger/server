6b84eebefd84413e4649d261b5c8acf7d3eceb8b
MetalMatze <me@metalmatze.de>
2014-09-14 01:50:31 +0200

    Create UserController with UserCest

---
+++ tests/acceptance/UserCest.php
@@ -0,0 +1,18 @@
+<?php
+use \AcceptanceTester;
+
+class UserCest
+{
+    public function _before()
+    {
+    }
+
+    public function _after()
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
