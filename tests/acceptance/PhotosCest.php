e8e8c622224b2c06fdf3812d9475139370317849
MetalMatze <me@metalmatze.de>
2014-09-13 19:37:14 +0200

    Need to rewrite photo upload acceptance tests again

--- tests/acceptance/PhotosCest.php
+++ tests/acceptance/PhotosCest.php
@@ -42,38 +42,4 @@ class PhotosCest
 		$I->seeResponseCodeIs(400);
 		$I->seeResponseContainsJson(array('message' => 'No file uploaded.'));
 	}
-
-	public function tryToUploadopePhoto(AcceptanceTester $I)
-	{
-		$this->authenticate($I);
-
-		$I->sendPOST(self::$resourceName, null, array('photo' => __DIR__ . '/../_data/094251.jpg'));
-
-		$photo = array(
-			'id' => 6,
-			'file_name' => '094251.jpg',
-			'file_size' => 1350912,
-			'file_mime_type' => 'image/jpeg',
-			'file_sha1' => '2d56023c13b9ccf2e4bc086cb9abee7beac12152',
-			'width' => 3264,
-			'height' => 2448,
-			'user_id' => 1,
-			'captured_at' => '2014:08:06 09:42:51'
-		);
-
-		$I->seeResponseCodeIs(201);
-		$I->seeResponseContainsJson($photo);
-
-		$I->seeFileFound('2d56023c13b9ccf2e4bc086cb9abee7beac12152.jpg', __DIR__ . '/../../photos/1');
-	}
-
-	public function tryToUploadPhotoTwice(AcceptanceTester $I)
-	{
-		$this->authenticate($I);
-
-		$I->sendPOST(self::$resourceName, null, array('photo' => __DIR__ . '/../_data/28.jpg'));
-
-		$I->seeResponseCodeIs(409);
-		$I->seeResponseContainsJson(array('message' => 'File already exists.'));
-	}
 }
