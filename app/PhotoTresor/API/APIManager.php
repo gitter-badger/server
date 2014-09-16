e3400ae8fa7f16c7c380f58cdb7abb6fba89f7a2
MetalMatze <me@metalmatze.de>
2014-09-16 02:15:15 +0200

    Add some prototype docs to APIManager

--- app/PhotoTresor/API/APIManager.php
+++ app/PhotoTresor/API/APIManager.php
@@ -38,3 +38,21 @@ class APIManager {
 	}
 
 }
+///*
+// * Request
+// */
+//$expandable = API::expand();
+//$fields = API::fields();
+
+///*
+// * Response
+// */
+//API::success();
+//API::success(201)->resource($data);
+//API::success()->data($data); // same as resource
+//
+//API::error();
+//API::error(401)->message("Can't touch this");
+//API::error()->type('PDOException');
+//API::error()->type($e); // Where $e is an Exception, set type only
+//API::error()->exception($e); // Where $e is an Exception, sets type and message
