7ee47e51253d17780419663bd5bf3a2b9211b8af
MetalMatze <me@metalmatze.de>
2014-09-24 23:40:14 +0200

    Create PhotoTresor\PhotoRepository

---
+++ app/repositories/PhotoRepository.php
@@ -0,0 +1,39 @@
+<?php
+namespace PhotoTresor\Repositories;
+
+use Photo;
+
+class PhotoRepository {
+
+    /**
+     * @var Photo
+     */
+    protected $photo;
+
+    public function __construct(Photo $photo)
+    {
+        $this->photo = $photo;
+    }
+
+    public function all(array $options)
+    {
+        $this->order();
+
+        if(isset($options['expand']) && $options['expand'] == 'user') {
+            $this->expandUser();
+        }
+
+        return $this->photo->get();
+    }
+
+    protected function expandUser()
+    {
+        $this->photo = $this->photo->with('User');
+    }
+
+    protected function order()
+    {
+        $this->photo = $this->photo->orderBy('captured_at', 'DESC');
+    }
+
+}
