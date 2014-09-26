4009bf140e37bbe4761d3593bb820534f28ba193
MetalMatze <me@metalmatze.de>
2014-09-26 19:00:47 +0200

    Call repository->delete($id) on service's delete

--- app/services/Service.php
+++ app/services/Service.php
@@ -48,7 +48,7 @@ abstract class Service implements ServiceInterface {
      */
     public function delete($id)
     {
-        // TODO: Implement delete() method.
+        $this->repository->delete($id);
     }
 
     /**
