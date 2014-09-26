0d0429812f20f657553bf7a01a3784db2781d6d0
MetalMatze <me@metalmatze.de>
2014-09-26 17:39:11 +0200

    Convert tabs to spaces

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -13,107 +13,107 @@ class UsersController extends \BaseController {
         $this->usersRepository = $usersRepository;
     }
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
         $users = $this->usersRepository->all();
-		return Response::apiSuccess($users);
-	}
+        return Response::apiSuccess($users);
+    }
 
 
-	/**
-	 * Create a new user.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		$user = User::create(Input::all());
-		$user->password = Input::get('password');
-		$user->save();
+    /**
+     * Create a new user.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        $user = User::create(Input::all());
+        $user->password = Input::get('password');
+        $user->save();
 
-		return Response::apiSuccess($user, 201);
-	}
-
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		try {
-			return User::findOrFail($id);
-		} catch (ModelNotFoundException $e) {
-			return $this->modelNotFoundResponse();
-		}
-	}
+        return Response::apiSuccess($user, 201);
+    }
 
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		try
-		{
-			$user = User::findOrFail($id);
-
-			$user->email = Input::get('email');
-			$user->username = Input::get('username');
-			$user->name_first = Input::get('name_first');
-			$user->name_last = Input::get('name_last');
-			$user->active = Input::get('active');
-			$user->quota = Input::get('quota');
-
-			$user->save();
-
-			return $user;
-		}
-		catch (ModelNotFoundException $e)
-		{
-			return $this->modelNotFoundResponse();
-		}
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        try {
+            return User::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return $this->modelNotFoundResponse();
+        }
+    }
 
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		try
-		{
-			$user = User::findOrFail($id);
-			$user->delete();
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        try
+        {
+            $user = User::findOrFail($id);
 
-			return Response::apiSuccess([]);
-		}
-		catch (ModelNotFoundException $e)
-		{
-			return $this->modelNotFoundResponse();
-		}
-	}
+            $user->email = Input::get('email');
+            $user->username = Input::get('username');
+            $user->name_first = Input::get('name_first');
+            $user->name_last = Input::get('name_last');
+            $user->active = Input::get('active');
+            $user->quota = Input::get('quota');
 
-	/**
-	 * @return mixed
-	 */
-	private function modelNotFoundResponse()
-	{
-		return Response::apiError(['message' => 'User not found.'], 404);
-	}
+            $user->save();
+
+            return $user;
+        }
+        catch (ModelNotFoundException $e)
+        {
+            return $this->modelNotFoundResponse();
+        }
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        try
+        {
+            $user = User::findOrFail($id);
+            $user->delete();
+
+            return Response::apiSuccess([]);
+        }
+        catch (ModelNotFoundException $e)
+        {
+            return $this->modelNotFoundResponse();
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    private function modelNotFoundResponse()
+    {
+        return Response::apiError(['message' => 'User not found.'], 404);
+    }
 
 }
 
