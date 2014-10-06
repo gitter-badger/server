fd36fbbbe41190e15bdf3003936fc35513751e7a
MetalMatze <me@metalmatze.de>
2014-10-07 01:16:47 +0200

    Rename Service to AbstractService

--- app/services/AbstractService.php
+++ app/services/AbstractService.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\MessageBag;
 
-abstract class Service implements ServiceInterface {
+abstract class AbstractService implements ServiceInterface {
 
     protected $errors;
 
