7d7a9b83eb52226337ab0df4adab057baf8260b9
MetalMatze <me@metalmatze.de>
2014-10-07 01:18:03 +0200

    UserRepository now implements RepositoryInterface

--- app/repositories/UsersRepository.php
+++ app/repositories/UsersRepository.php
@@ -1,10 +1,9 @@
 <?php
-
 namespace PhotoTresor\Repositories;
 
 use User;
 
-class UsersRepository {
+class UsersRepository implements RepositoryInterface {
 
     protected $user;
 
@@ -13,9 +12,54 @@ class UsersRepository {
         $this->user = $user;
     }
 
+    /**
+     * @return Collection
+     */
     public function all()
     {
         return $this->user->get();
     }
 
-} 
+    /**
+     * @param int $id
+     * @return Model
+     */
+    public function find($id)
+    {
+        return $this->user->findOrFail($id);
+    }
+
+    /**
+     * @param array $input
+     * @return Model
+     */
+    public function create(array $input)
+    {
+        return $this->user->create($input);
+    }
+
+    /**
+     * @param array $input
+     * @return Model
+     */
+    public function update(array $input)
+    {
+        $user = $this->find($input['id']);
+
+        $user->update($input);
+
+        return $user;
+    }
+
+    /**
+     * @param int $id
+     * @return boolean
+     */
+    public function delete($id)
+    {
+        $user = $this->find($id);
+
+        return $user->delete();
+    }
+
+}
--- tests/unit/UsersRepositoryTest.php
+++ tests/unit/UsersRepositoryTest.php
@@ -4,23 +4,72 @@ use PhotoTresor\Repositories\UsersRepository;
 
 class UsersRepositoryTest extends TestCase
 {
-    private $userModel;
-    private $usersRepository;
+    private $model;
+    private $repository;
 
     public function setUp()
     {
         parent::setUp();
 
         Mockery::mock('Eloquent');
-        $this->userModel = Mockery::mock('User');
-        $this->usersRepository = new UsersRepository($this->userModel);
+        $this->model = Mockery::mock('User');
+        $this->repository = new UsersRepository($this->model);
     }
 
     public function testAll()
     {
-        $this->userModel->shouldReceive('get')->once()->withNoArgs()->andReturn($this->userModel);
+        $this->model->shouldReceive('get')->once()->withNoArgs()->andReturn($this->model);
 
-        $this->usersRepository->all();
+        $returned = $this->repository->all();
+
+        $this->assertSame($this->model, $returned);
+    }
+
+    public function testFind()
+    {
+        $this->mockFindOrFail();
+
+        $returned = $this->repository->find(1234);
+
+        $this->assertSame($this->model, $returned);
+    }
+
+    public function testCreate()
+    {
+        $inputs = ['email' => 'matthias@phototresor.org'];
+
+        $this->model->shouldReceive('create')->once()->with($inputs)->andReturn($this->model);
+
+        $returned = $this->repository->create($inputs);
+
+        $this->assertSame($this->model, $returned);
+    }
+
+    public function testUpdate()
+    {
+        $inputs = ['id' => 1234, 'email' => 'matthias@phototresor.org'];
+
+        $this->mockFindOrFail();
+        $this->model->shouldReceive('update')->once()->with($inputs)->andReturn($this->model);
+
+        $returned = $this->repository->update($inputs);
+
+        $this->assertSame($this->model, $returned);
+    }
+
+    public function testDelete()
+    {
+        $this->mockFindOrFail();
+        $this->model->shouldReceive('delete')->once()->withNoArgs()->andReturn($this->model);
+
+        $returned = $this->repository->delete(1234);
+
+        $this->assertSame($this->model, $returned);
+    }
+
+    private function mockFindOrFail()
+    {
+        $this->model->shouldReceive('findOrFail')->once()->with(1234)->andReturn($this->model);
     }
 
 }
