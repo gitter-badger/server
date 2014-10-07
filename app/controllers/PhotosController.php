1de7b6f215f71f1147590f48b97fbf9cb03d080f
MetalMatze <me@metalmatze.de>
2014-10-07 02:09:23 +0200

    convert tabs to spaces in PhotosController

--- app/controllers/PhotosController.php
+++ app/controllers/PhotosController.php
@@ -14,14 +14,14 @@ class PhotosController extends \BaseController {
         $this->photos = $photos;
     }
 
-	/**
-	 * Display a listing of the resource.
-	 * GET /photos
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
+    /**
+     * Display a listing of the resource.
+     * GET /photos
+     *
+     * @return Response
+     */
+    public function index()
+    {
         if(Input::get('expand') == 'user')
         {
             $photos = $this->photos->allWithUsers();
@@ -29,101 +29,101 @@ class PhotosController extends \BaseController {
             $photos = $this->photos->all();
         }
 
-		return Response::apiSuccess($photos);
-	}
+        return Response::apiSuccess($photos);
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /photos
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		if (Input::hasFile('photo'))
-		{
-			$inputImage = Input::file('photo');
+    /**
+     * Store a newly created resource in storage.
+     * POST /photos
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        if (Input::hasFile('photo'))
+        {
+            $inputImage = Input::file('photo');
 
-			if ($inputImage->isValid())
-			{
-				$interventionImage = Image::make($inputImage);
+            if ($inputImage->isValid())
+            {
+                $interventionImage = Image::make($inputImage);
 
-				$user_id =  Auth::user()->id;
-				$file_sha1 = sha1_file($inputImage->getRealPath());
+                $user_id =  Auth::user()->id;
+                $file_sha1 = sha1_file($inputImage->getRealPath());
 
-				$fileExists = Photo::where('user_id', '=', $user_id)->where('file_sha1', '=', $file_sha1)->count();
+                $fileExists = Photo::where('user_id', '=', $user_id)->where('file_sha1', '=', $file_sha1)->count();
 
-				if($fileExists)
-					return Response::json(array('message' => 'File already exists.'), 409);
+                if($fileExists)
+                    return Response::json(array('message' => 'File already exists.'), 409);
 
-				$photo = Photo::create([
-					'file_name' => $inputImage->getClientOriginalName(),
-					'file_size' => $inputImage->getSize(),
-					'file_mime_type' => $interventionImage->mime(),
-					'file_sha1' => $file_sha1,
-					'width' => $interventionImage->width(),
-					'height' => $interventionImage->height(),
-					'user_id' => $user_id,
-					'captured_at' => $interventionImage->exif()['DateTimeOriginal']
-				]);
+                $photo = Photo::create([
+                    'file_name' => $inputImage->getClientOriginalName(),
+                    'file_size' => $inputImage->getSize(),
+                    'file_mime_type' => $interventionImage->mime(),
+                    'file_sha1' => $file_sha1,
+                    'width' => $interventionImage->width(),
+                    'height' => $interventionImage->height(),
+                    'user_id' => $user_id,
+                    'captured_at' => $interventionImage->exif()['DateTimeOriginal']
+                ]);
 
-				$file_path = Config::get('phototresor.storage') . "/$user_id/";
-				$file_name = "$photo->file_sha1.jpg";
-				$inputImage->move($file_path, $file_name);
+                $file_path = Config::get('phototresor.storage') . "/$user_id/";
+                $file_name = "$photo->file_sha1.jpg";
+                $inputImage->move($file_path, $file_name);
 
-				return Response::json($photo, 201);
-			}
-		} else {
-			return Response::json(array('message' => 'No file uploaded.'), 400);
-		}
-	}
+                return Response::json($photo, 201);
+            }
+        } else {
+            return Response::json(array('message' => 'No file uploaded.'), 400);
+        }
+    }
 
-	/**
-	 * Display the specified resource.
-	 * GET /photos/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		try
-		{
-			return Photo::findOrFail($id);
-		}
-		catch (ModelNotFoundException $e)
-		{
-			return Response::apiError(['message' => 'Photo not found.'], 404);
-		}
-	}
+    /**
+     * Display the specified resource.
+     * GET /photos/{id}
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        try
+        {
+            return Photo::findOrFail($id);
+        }
+        catch (ModelNotFoundException $e)
+        {
+            return Response::apiError(['message' => 'Photo not found.'], 404);
+        }
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /photos/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     * PUT /photos/{id}
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /photos/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		$photo = Photo::find($id);
-		$photoPath = Config::get('phototresor.storage') . "$photo->user_id/$photo->file_sha1.jpg";
-		Log::debug('Delete Photo: ' . $photoPath);
+    /**
+     * Remove the specified resource from storage.
+     * DELETE /photos/{id}
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        $photo = Photo::find($id);
+        $photoPath = Config::get('phototresor.storage') . "$photo->user_id/$photo->file_sha1.jpg";
+        Log::debug('Delete Photo: ' . $photoPath);
 
-		File::delete($photoPath);
-		return Photo::destroy($id);
-	}
+        File::delete($photoPath);
+        return Photo::destroy($id);
+    }
 
 }
