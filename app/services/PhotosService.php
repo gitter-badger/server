f8e175e4d2e83542d9b1835bb5b6e67cb20e8ba3
MetalMatze <me@metalmatze.de>
2014-10-10 02:00:07 +0200

    Create delete method for PhotoService that also delete photo from disk

--- app/services/PhotosService.php
+++ app/services/PhotosService.php
@@ -1,21 +1,62 @@
 <?php
 namespace PhotoTresor\Services;
 
+use Illuminate\Config\Repository as ConfigRepository;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Filesystem\FileNotFoundException;
+use Illuminate\Filesystem\Filesystem;
 use PhotoTresor\Repositories\PhotosRepository;
-use PhotoTresor\Repositories\RepositoryInterface;
 use PhotoTresor\Validators\PhotosValidator;
-use PhotoTresor\Validators\ValidatorInterface;
 
 class PhotosService extends AbstractService {
 
-    public function __construct(PhotosRepository $photosRepository, PhotosValidator $photosValidator)
+    /**
+     * @var ConfigRepository
+     */
+    private $config;
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    public function __construct(
+        PhotosRepository $photosRepository,
+        PhotosValidator $photosValidator,
+        ConfigRepository $config,
+        Filesystem $filesystem
+    )
     {
         parent::__construct($photosRepository, $photosValidator);
+
+        $this->config = $config;
+        $this->filesystem = $filesystem;
     }
 
     public function allWithUsers()
     {
         return $this->repository->allWithUsers();
+    }
+
+    /**
+     * Delete a photo from the filesystem and the database.
+     *
+     * @param int $id
+     * @return bool
+     * @throws FileNotFoundException
+     * @throws ModelNotFoundException
+     */
+    public function delete($id)
+    {
+        $photo = $this->repository->find($id);
+
+        $path = $this->config->get('phototresor.storage') . "$photo->user_id/$photo->file_sha1.jpg";
+
+        if( ! $this->filesystem->delete($path))
+        {
+            throw new FileNotFoundException();
+        }
+
+        return $this->repository->delete($id);
     }
 
 }
---
+++ tests/unit/services/PhotosServiceTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use PhotoTresor\Services\PhotosService;
+
+class PhotosServiceTest extends TestCase
+{
+    private $config;
+    private $filesystem;
+    private $model;
+    private $repository;
+    private $service;
+    private $validator;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->repository = Mockery::mock('PhotoTresor\Repositories\PhotosRepository');
+        $this->validator = Mockery::mock('PhotoTresor\Validators\PhotosValidator');
+        $this->config = Mockery::mock('Illuminate\Config\Repository');
+        $this->filesystem = Mockery::mock('Illuminate\Filesystem\Filesystem');
+
+        $this->service = new PhotosService($this->repository, $this->validator, $this->config, $this->filesystem);
+
+        $this->model = (object) ['user_id' => 1, 'file_sha1' => 'abcdef'];
+    }
+
+    public function testDelete()
+    {
+        $this->repository->shouldReceive('find')->once()->with(1234)->andReturn($this->model);
+        $this->config->shouldReceive('get')->once()->with('phototresor.storage')->andReturn('photos/');
+        $this->filesystem->shouldReceive('delete')->once()->with('photos/1/abcdef.jpg')->andReturn(true);
+        $this->repository->shouldReceive('delete')->once()->with(1234)->andReturn(true);
+
+        $returned = $this->service->delete(1234);
+
+        $this->assertTrue($returned);
+    }
+
+    /**
+     * @expectedException \Illuminate\Filesystem\FileNotFoundException
+     */
+    public function testDeleteFileNotFoundException()
+    {
+        $this->repository->shouldReceive('find')->once()->with(1234)->andReturn($this->model);
+        $this->config->shouldReceive('get')->once()->with('phototresor.storage')->andReturn('photos/');
+        $this->filesystem->shouldReceive('delete')->once()->with('photos/1/abcdef.jpg')->andReturn(false);
+
+        $this->service->delete(1234);
+    }
+
+}
+
